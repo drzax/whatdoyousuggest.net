48c7dcf0bdc735799f1bb74163f37c790ba7e6c3
Simon Elvery <simon@elvery.net>
2018-03-16 21:59:51 +1000

    Delete old stuff

--- proxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// File Name: proxy.php
-if (!isset($_GET['url'])) die();
-$url = urldecode($_GET['url']);
-$url = 'http://' . str_replace('http://', '', $url); // Avoid accessing the file system
-echo utf8_encode(file_get_contents($url));
-die;
-$xml = simplexml_load_string( utf8_encode(file_get_contents($url)) );
-echo json_encode(new SimpleXMLElement($xml->asXML(), LIBXML_NOCDATA));
