7b192a8cc38f774bde59b98a417d972ebd9aa804
Simon Elvery <simon@elvery.net>
2021-02-08 11:00:57 +1000

    Disable function response caching.

--- functions/query.js
+++ functions/query.js
@@ -14,7 +14,9 @@ exports.handler = async ({ queryStringParameters }, context) => {
   return {
     statusCode: 200,
     headers: {
-      'Cache-Control': 'public, max-age=604800, s-maxage=604800'
+      // Netlify, doesn't currently use query strings in the cache key: https://community.netlify.com/t/netlify-function-with-query-strings-ignores-custom-cache-control-header/15390/20
+      // 'Cache-Control': 'public, max-age=604800, s-maxage=604800',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(results)
   };
