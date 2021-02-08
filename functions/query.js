6e538e1f2ee6b0caed4c4b31f6844ffeb0d953d2
Simon Elvery <simon@elvery.net>
2021-02-08 17:51:46 +1000

    Fix function after updating got dependency

--- functions/query.js
+++ functions/query.js
@@ -2,9 +2,9 @@ const got = require('got');
 
 exports.handler = async ({ queryStringParameters }, context) => {
   const results = await got('http://google.com/complete/search', {
-    json: true,
+    responseType: 'json',
     // TODO: Maybe whitelist the query params here
-    query: Object.assign({ client: 'chrome' }, queryStringParameters)
+    searchParams: Object.assign({ client: 'chrome' }, queryStringParameters)
   }).then(res => {
     return res.body[1].filter((d, i) => {
       return res.body[4]['google:suggesttype'][i] === 'QUERY';
