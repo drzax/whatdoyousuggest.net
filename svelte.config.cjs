73fe5ed398dc9c41a63c9e394a3ac17f1340efef
Simon Elvery <simon@elvery.net>
2021-03-13 12:15:04 +1000

    update config files

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,5 @@
 const sveltePreprocess = require("svelte-preprocess");
+/** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
@@ -11,10 +12,5 @@ module.exports = {
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-
-    prerender: {
-      enabled: true,
-      pages: ["/about/privacy"],
-    },
   },
 };
